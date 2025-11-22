3c80782ef1142f5a949450ba444f6fa1871dc1db
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 01:48:49 +0700

    Update Transaksi.java

--- Transaksi.java
+++ Transaksi.java
@@ -6,31 +6,38 @@ public class Transaksi {
     private int idTransaksi;
     private Pesanan pesanan;
     private Pembayaran metodePembayaran;
-    private boolean statusKonfirmasi; // Tambahan untuk cek sukses
+    private boolean statusKonfirmasi; 
 
+    // Konstruktor Transaksi
     public Transaksi(int idTransaksi, Pesanan pesanan, Pembayaran metodePembayaran) {
         this.idTransaksi = idTransaksi;
         this.pesanan = pesanan;
         this.metodePembayaran = metodePembayaran;
-        this.statusKonfirmasi = false; // Awalnya false
+        this.statusKonfirmasi = false; // Status konfirmasi awal adalah false
     }
-    
+
+    // Memulai proses pembayaran dan mengkonfirmasi status transaksi
     public void konfirmasi(Scanner sc) {
     System.out.println("Memulai proses pembayaran...");
+    // Memanggil metode pembayaran spesifik
     boolean sukses = metodePembayaran.idPembayaran(pesanan.hitungTotal(), sc); 
 
+    // Mengatur status Transaksi dan Pesanan berdasarkan hasil pembayaran
     if (sukses) {
         this.statusKonfirmasi = true;
-        this.pesanan.setStatus("Lunas");
+        this.pesanan.setStatus("Lunas"); // Ubah status Pesanan menjadi Lunas
     } else {
         this.statusKonfirmasi = false;
     }
 }
-    // Getters untuk Struk
+    // Getters untuk Struk dan informasi
+    // Mengembalikan status konfirmasi transaksi
     public boolean isStatusKonfirmasi() { return statusKonfirmasi; }
+    // Mengembalikan objek Pesanan
     public Pesanan getPesanan() { return pesanan; }
+    // Mengembalikan objek metode Pembayaran
     public Pembayaran getMetodePembayaran() { return metodePembayaran; }
-
+    // Mengembalikan ID Transaksi
     public int getIdTransaksi() { 
         return this.idTransaksi; 
     }
