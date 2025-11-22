ff842ed7ed048ed2fa34b6d0ce80363bc1468051
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 00:34:59 +0700

    Update MenuItem.java

--- MenuItem.java
+++ MenuItem.java
@@ -2,19 +2,23 @@ package dummyUASLabPBOB_3;
 
 public abstract class MenuItem {
     private String nama;
-    private int harga; // Sesuai diagram: int
+    private int harga; 
 
+    // Konstruktor MenuItem
     public MenuItem(String nama, int harga) {
         this.nama = nama;
         this.harga = harga;
     }
 
+    // Mengembalikan nama item
     public String getNama() { return nama; }
+    // Mengembalikan harga item
     public int getHarga() { return harga; }
 
-    // Sesuai diagram: +getInfo()
+    // Metode abstrak. Wajib diimplementasikan oleh subclass untuk memberikan detail info item
     public abstract String getInfo();
-    
+
+    // Mengembalikan string yang berisi detail info item
     @Override
     public String toString() {
         return getInfo();
