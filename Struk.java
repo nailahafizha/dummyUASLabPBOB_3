e33982657d7053252885ebafe618ca1bdedebd92
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 01:37:56 +0700

    Update Struk.java

--- Struk.java
+++ Struk.java
@@ -2,35 +2,40 @@ package dummyUASLabPBOB_3;
 
 public class Struk {
     
-    // Sesuai diagram: +Cetak(Transaksi)
+    // Mencetak struk pembayaran untuk Transaksi
     public void Cetak(Transaksi transaksi) {
+        // Cek status konfirmasi, struk gagal dicetak jika pembayaran belum lunas
         if (!transaksi.isStatusKonfirmasi()) {
             System.out.println("STRUK GAGAL DICETAK (Pembayaran belum lunas)");
             return;
         }
-        
+
+        // Ambil objek Pesanan dari Transaksi
         Pesanan p = transaksi.getPesanan();
         
         System.out.println("\n========================================");
         System.out.println("              STRUK PEMBAYARAN");
         System.out.println("========================================");
-        System.out.println("ID Transaksi: " + transaksi.getIdTransaksi()); // Ganti jadi getter
+        System.out.println("ID Transaksi: " + transaksi.getIdTransaksi()); 
         System.out.println("ID Pesanan  : " + p.getIdPesanan());
-        System.out.println("Meja No.    : " + p.getMeja().getNomor());
+        System.out.println("Meja No.    : " + p.getMeja().getNomor()); 
         System.out.println("Metode Bayar: " + transaksi.getMetodePembayaran().getNamaMetode());
         System.out.println("----------------------------------------");
-        
+
+        // Iterasi melalui setiap item dalam Pesanan
         for(DetailPesanan d : p.getDaftarItem()) {
             System.out.printf("%-20s x%d \t Rp %d\n", 
                 d.getItem().getNama(), 
                 d.getJumlah(), 
                 d.getSubtotal());
+            // Tampilkan catatan jika ada
             if (!d.getCatatan().equals("-")) {
                  System.out.println("  > Catatan: " + d.getCatatan());
             }
         }
         
         System.out.println("----------------------------------------");
+        // Menghitung dan menampilkan total akhir
         System.out.printf("TOTAL BAYAR: \t\t\t Rp %d\n", p.hitungTotal());
         System.out.println("STATUS: LUNAS");
         System.out.println("========================================");
