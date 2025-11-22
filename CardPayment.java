fcf22be9b0daeecd8980f57c11536430b5a978a3
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-22 23:14:39 +0700

    Update CardPayment.java

--- CardPayment.java
+++ CardPayment.java
@@ -2,13 +2,17 @@ package dummyUASLabPBOB_3;
 
 import java.util.Scanner;
 
+// Implementasi pembayaran menggunakan Kartu Kredit/Debit
 public class CardPayment implements Pembayaran {
+
+    // Proses pembayaran kartu
     @Override
     public boolean idPembayaran(double total, Scanner sc) {
         System.out.println("Memproses Kartu Kredit/Debit sebesar Rp " + total);
-        return true;
+        return true; // Asumsi berhasil
     }
-    
+
+    // Mengembalikan nama metode "Card"
     @Override
     public String getNamaMetode() { return "Card"; }
 }
