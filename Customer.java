f0a261ed979896a6dd7bb01725d824c2a58d3640
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-22 23:31:21 +0700

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,19 +1,16 @@
 package dummyUASLabPBOB_3;
 
-
 public class Customer extends Akun {
+
+    // Konstruktor Customer. Memanggil konstruktor superclass Akun
     public Customer(int id, String nama, String password) {
         super(id, nama, password);
     }
 
-    // Sesuai diagram: +buatPesanan()
-    // Method ini membuat dan mengembalikan objek Pesanan baru
+    // Membuat dan mengembalikan objek Pesanan baru
     public Pesanan buatPesanan(int idPesanan, Meja meja) {
         System.out.println("Customer " + getNama() + " membuat pesanan baru (ID: " + idPesanan + ").");
         
-        // [PERBAIKAN DI SINI]
-        // Kita tambahkan 'this' (objek Customer ini)
-        // agar constructor Pesanan yang baru (3 argumen) terpenuhi
         return new Pesanan(idPesanan, meja, this); 
     }
 }
