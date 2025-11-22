45a5db04b35ce8c775321f235414fb7f2e588b21
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 01:22:32 +0700

    Update QRISPayment.java

--- QRISPayment.java
+++ QRISPayment.java
@@ -2,15 +2,18 @@ package dummyUASLabPBOB_3;
 
 import java.util.Scanner;
 
+// Implementasi pembayaran menggunakan QRIS
 public class QRISPayment implements Pembayaran {
+
+    // Memproses pembayaran QRIS
     @Override
     public boolean idPembayaran(double total, Scanner sc) {
         System.out.println("Silakan scan QRIS untuk pembayaran Rp " + total);
         System.out.println("... (Menunggu Konfirmasi) ... Pembayaran Berhasil.");
-        return true;
+        return true; // Asumsi pembayaran QRIS berhasil setelah discan
     }
-    
+
+    // Mengembalikan nama metode pembayaran "QRIS"
     @Override
     public String getNamaMetode() { return "QRIS"; }
 }
-
