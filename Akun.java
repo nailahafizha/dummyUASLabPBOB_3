331d135544c2d8fcea61d1e1f19884f5461191f2
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-22 23:06:25 +0700

    Update Akun.java

--- Akun.java
+++ Akun.java
@@ -5,13 +5,17 @@ public abstract class Akun {
     private String nama;
     private String password;
 
+    // Konstruktor Akun. Dimana menginisialisasi ID, nama, dan password
     public Akun(int id, String nama, String password) {
         this.id = id;
         this.nama = nama;
         this.password = password;
     }
 
+    // Mengembalikan ID
     public int getId() { return id; }
+    // Mengembalikan nama
     public String getNama() { return nama; }
+    // Mengembalikan password
     public String getPassword() { return password; }
 }
