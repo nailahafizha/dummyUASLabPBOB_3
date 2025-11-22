241bedcfe452a73072b64aa6dfe4e51b246a38ea
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 00:23:17 +0700

    Update Meja.java

--- Meja.java
+++ Meja.java
@@ -4,12 +4,16 @@ public class Meja {
     private int nomor;
     private String status;
 
+    // Konstruktor Meja
     public Meja(int nomor) {
         this.nomor = nomor;
         this.status = "Terisi"; // Otomatis terisi saat dibuat
     }
 
+    // Mengembalikan nomor meja
     public int getNomor() { return nomor; }
+    // Mengembalikan status meja
     public String getStatus() { return status; }
+    // Mengubah status meja
     public void setStatus(String status) { this.status = status; }
 }
