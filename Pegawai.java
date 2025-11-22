95a5ea94b608d88ce8155414c2574d40d13fc54e
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 00:50:34 +0700

    Update Pegawai.java

--- Pegawai.java
+++ Pegawai.java
@@ -3,14 +3,16 @@ package dummyUASLabPBOB_3;
 public class Pegawai extends Akun {
     private String peran;
 
+    // Konstruktor
     public Pegawai(int id, String nama, String password, String peran) {
-        super(id, nama, password);
+        super(id, nama, password); // Panggil konstruktor superclass Akun
         this.peran = peran;
     }
 
+    // Mengembalikan peran pegawai
     public String getPeran() { return peran; }
 
-    // Sesuai diagram: +updateStatusPesanan()
+    // Mengubah status Pesanan
     public void updateStatusPesanan(Pesanan pesanan, String status) {
         pesanan.setStatus(status);
         System.out.println("Status pesanan " + pesanan.getIdPesanan() + " diupdate menjadi: " + status);
