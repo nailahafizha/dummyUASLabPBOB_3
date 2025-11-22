034f16a5bb3187ad692df9ddf341185eb8a9ecd6
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 00:40:45 +0700

    Update Minuman.java

--- Minuman.java
+++ Minuman.java
@@ -4,13 +4,14 @@ public class Minuman extends MenuItem {
     private String ukuran;
     private String suhu;
 
+    // Konstruktor. Menginisialisasi nama, harga, ukuran, dan suhu
     public Minuman(String nama, int harga, String ukuran, String suhu) {
-        super(nama, harga);
+        super(nama, harga); // Panggil konstruktor superclass
         this.ukuran = ukuran;
         this.suhu = suhu;
     }
 
-    // Implementasi method abstract
+    // Implementasi getInfo(). Mengembalikan deskripsi lengkap Minuman
     @Override
     public String getInfo() {
         return String.format("%-20s (%s, %s) - Rp %d", getNama(), ukuran, suhu, getHarga());
