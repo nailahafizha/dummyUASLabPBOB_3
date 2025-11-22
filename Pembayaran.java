48ca4ee6118cc7cc96769b78a03b08479a1d84aa
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 00:57:44 +0700

    Update Pembayaran.java

--- Pembayaran.java
+++ Pembayaran.java
@@ -2,9 +2,12 @@ package dummyUASLabPBOB_3;
 
 import java.util.Scanner;
 
+// Interface Pembayaran. Kontrak untuk semua metode pembayaran
 public interface Pembayaran {
-    // Diagram bilang 'idPembayaran', tapi itu nama yg aneh untuk method
-    // Kita ganti jadi 'lakukanPembayaran'
+    
+    // Memproses transaksi dan mengembalikan status keberhasilan (true/false)
     boolean idPembayaran(double total, Scanner sc);
+
+    // Mengembalikan nama metode pembayaran
     String getNamaMetode();
 }
