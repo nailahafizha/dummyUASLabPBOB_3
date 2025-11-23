59dfb833440a08fda0bb7cb2bb6e05ab37f103a3
nailahafizha <naila241@mhs.usk.ac.id>
2025-11-23 11:50:54 +0700

    Update RestaurantSystem.java

--- RestaurantSystem.java
+++ RestaurantSystem.java
@@ -6,13 +6,12 @@ import java.util.stream.Collectors;
 
 public class RestaurantSystem {
     
-    // Sesuai diagram:
+    // Daftar data utama
     private List<MenuItem> daftarMenu;
     private List<Pegawai> daftarPegawai;
-    private List<Pesanan> daftarPesanan;
-    
-    // Tambahan untuk login
     private List<Customer> daftarCustomer;
+    // Daftar pesanan aktif
+    private List<Pesanan> daftarPesanan;
 
     public RestaurantSystem() {
         // Load semua data dari file saat sistem dinyalakan
@@ -22,12 +21,12 @@ public class RestaurantSystem {
         this.daftarPesanan = new ArrayList<>(); // Pesanan dimulai kosong
     }
 
-    // Sesuai diagram: +tambahPesanan()
-    // @Override  <-- HAPUS BARIS INI
+    // Menambahkan objek Pesanan baru ke daftar pesanan aktif
     public void tambahPesanan(Pesanan pesanan) {
         this.daftarPesanan.add(pesanan);
     }
 
+    // Menampilkan semua item dalam menu
     public void lihatMenu() {
         System.out.println("\n--- Daftar Menu Restoran ---");
         for (int i = 0; i < daftarMenu.size(); i++) {
@@ -38,10 +37,12 @@ public class RestaurantSystem {
     
     // --- Method Helper untuk Driver ---
     
+    // Mengembalikan seluruh daftar pesanan aktif
     public List<Pesanan> getDaftarPesanan() {
         return this.daftarPesanan;
     }
-    
+
+    // Memproses login
     public Akun login(int id, String password) {
         // Cek di pegawai
         for (Pegawai p : daftarPegawai) {
@@ -57,7 +58,8 @@ public class RestaurantSystem {
         }
         return null; // Tidak ketemu
     }
-    
+
+    // Mendaftarkan Customer baru
     public Customer registerCustomer(String nama, String pass) {
         // Buat ID baru (ID customer terakhir + 1)
         int newId = 101; // default
@@ -78,7 +80,8 @@ public class RestaurantSystem {
         }
         return null;
     }
-    
+
+    // Mencari dan mengembalikan objek Pesanan berdasarkan ID Pesanan
     public Pesanan findPesananById(int id) {
         for (Pesanan p : daftarPesanan) {
             if (p.getIdPesanan() == id) {
@@ -87,9 +90,10 @@ public class RestaurantSystem {
         }
         return null;
     }
-    
+
+    // Mengambil List Pesanan yang memiliki status tertentu (misal "Dipesan")
     public List<Pesanan> getDaftarPesananByStatus(String status) {
-        // Pakai Java Stream untuk filter
+        // Menggunakan Java Stream untuk memfilter daftar pesanan
         return daftarPesanan.stream()
             .filter(p -> p.getStatus().equalsIgnoreCase(status))
             .collect(Collectors.toList());
